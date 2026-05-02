261be7d2618acdf94d48e4397705d298f16938a8
kizerfig <kizerelliooth@gmail.com>
2026-05-02 01:00:32 -0400

    probandorama

--- P1/src/app/search.tsx
+++ P1/src/app/search.tsx
@@ -14,6 +14,7 @@ import { Camera, useCameraDevice, useCameraPermission } from 'react-native-visio
 import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useEffect, useRef, useState } from 'react';
 
+
 // ─── Constantes de configuración ─────────────────────────────────────────────
 
 /**
